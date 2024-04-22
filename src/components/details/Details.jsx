238878ef48074382075d260b2f7a91fa91243f42
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-04-22 12:34:16 +0600

    chitchat app created

---
+++ src/components/details/Details.jsx
@@ -0,0 +1,11 @@
+import './details.css';
+
+const Details = () => {
+    return (
+        <div className='details'>
+            details
+        </div>
+    );
+};
+
+export default Details;
---
+++ src/components/list/List.jsx
@@ -0,0 +1,14 @@
+import ChatList from './chatList/ChatList';
+import './list.css';
+import UserInfo from './userInfo/UserInfo';
+
+const List = () => {
+    return (
+        <div className='list'>
+           <UserInfo></UserInfo>
+           <ChatList></ChatList> 
+        </div>
+    );
+};
+
+export default List;
