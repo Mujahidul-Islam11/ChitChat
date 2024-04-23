98c5ef36d22b1f4a3937daf433c9903203021a0f
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-04-23 22:33:34 +0600

    Chat section completed

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -6,10 +6,10 @@ const Chat = () => {
   const [emojiHandler, setEmojiHandler] = useState(false);
   const [text, setText] = useState("");
 
-  const handleEmoji = e =>  {
-    setText((prev) => prev + e.emoji)
-    setEmojiHandler(false)
-  }
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setEmojiHandler(false);
+  };
 
   return (
     <div className="chat">
@@ -28,7 +28,53 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex
+              ullam vel sequi, voluptatibus cumque tempora aut praesentium
+              quibusdam tempore cum placeat molestiae rem minima exercitationem
+              iusto vero laudantium id?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex
+              ullam vel sequi, voluptatibus cumque tempora aut praesentium
+              quibusdam tempore cum placeat molestiae rem minima exercitationem
+              iusto vero laudantium id?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet 
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="./avatar.png" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex
+              ullam vel sequi, voluptatibus cumque tempora aut praesentium
+              quibusdam tempore cum placeat molestiae rem minima exercitationem
+              iusto vero laudantium id?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
 
       <div className="bottom">
         <div className="icons">
@@ -36,7 +82,12 @@ const Chat = () => {
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <div className="emoji">
           <img
             src="./emoji.png"
@@ -44,7 +95,11 @@ const Chat = () => {
             onClick={() => setEmojiHandler(!emojiHandler)}
           />
           <div className="picker">
-          {emojiHandler? <EmojiPicker onEmojiClick={handleEmoji}></EmojiPicker> : ""}
+            {emojiHandler ? (
+              <EmojiPicker onEmojiClick={handleEmoji}></EmojiPicker>
+            ) : (
+              ""
+            )}
           </div>
         </div>
         <button className="sendButton">Send</button>
